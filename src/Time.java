c50ce91508457350f71d3648296523497739d7d0
Alyson Fernandes Basilio <alyson.basilio@gmail.com>
2016-04-18 14:59:04 -0300

    Classe original

---
+++ src/Time.java
@@ -0,0 +1,14 @@
+
+public class Time {
+	int hour;
+	int minute;
+	
+	Time(){};
+	
+	public static void main (String arg[]){
+		Time t = new Time();
+		t.hour = 3;
+		t.minute = 25;
+		System.out.println("The time now is "+t.hour+":"+t.minute);
+	}
+}
