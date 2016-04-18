82d4f61e1aaab3f4c044bececac7fc513b12e5b9
Alyson Fernandes Basilio <alyson.basilio@gmail.com>
2016-04-18 15:06:08 -0300

    Alteração para implementar o encapsulamento
    O Encapsulamento previne o acesso direto às variáveis. Observe que para
    acessar a data e a hora, é necessário acionar um método.

--- src/Time.java
+++ src/Time.java
@@ -1,14 +1,23 @@
 
 public class Time {
-	int hour;
-	int minute;
+	private int hour;
+	private int minute;
 	
-	Time(){};
+	Time(int h, int m){
+		hour = h;
+		minute = m;
+	};
+	
+	public int getHour(){
+		return hour;
+	}
+	
+	public int getMinute(){
+		return minute;
+	}
 	
 	public static void main (String arg[]){
-		Time t = new Time();
-		t.hour = 3;
-		t.minute = 25;
-		System.out.println("The time now is "+t.hour+":"+t.minute);
+		Time t = new Time(3,25);
+		System.out.println("The time now is "+t.getHour()+":"+t.getMinute());
 	}
 }
